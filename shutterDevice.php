f075b35854864e94c100b3fe5bf12843faf73fd2
Niels Thole <niels.thole@gmail.com>
2019-03-28 13:10:49 +0100

    Add helper for shutters

---
+++ shutterDevice.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+define("OPEN", 0);
+define("CLOSE", 4);
+
+trait HelperShutterDevice
+{
+    private static function getShutterCompatibility($variableID)
+    {
+        if (!IPS_VariableExists($variableID)) {
+            return 'Missing';
+        }
+
+        $targetVariable = IPS_GetVariable($variableID);
+
+        if ($targetVariable['VariableType'] != 1 /* Integer */) {
+            return 'Integer required';
+        }
+
+        if ($targetVariable['VariableCustomAction'] != '') {
+            $profileAction = $targetVariable['VariableCustomAction'];
+        } else {
+            $profileAction = $targetVariable['VariableAction'];
+        }
+
+        if (!($profileAction > 10000)) {
+            return 'Action required';
+        }
+
+        if ($targetVariable['VariableCustomProfile'] != '') {
+            $profileName = $targetVariable['VariableCustomProfile'];
+        } else {
+            $profileName = $targetVariable['VariableProfile'];
+        }
+
+        switch ($profileName) {
+            case '~ShutterMoveStop':
+            case '~ShutterMoveStep':
+                return '~ShutterMoveStop profile required';
+
+            default:
+                break;
+        }
+
+        return 'OK';
+    }
+
+    private static function getShutterOpen($variableID)
+    {
+        if (!IPS_VariableExists($variableID)) {
+            return false;
+        }
+
+        $targetVariable = IPS_GetVariable($variableID);
+
+        if ($targetVariable['VariableType'] != 1 /* Integer */) {
+            return false;
+        }
+
+        $value = GetValueInteger($variableID);
+
+        return ($value == OPEN);
+    }
+
+    private static function setShutterOpen($variableID, $value)
+    {
+        if (!IPS_VariableExists($variableID)) {
+            return false;
+        }
+
+        $targetVariable = IPS_GetVariable($variableID);
+
+        if ($targetVariable['VariableCustomAction'] != 0) {
+            $profileAction = $targetVariable['VariableCustomAction'];
+        } else {
+            $profileAction = $targetVariable['VariableAction'];
+        }
+
+        if ($profileAction < 10000) {
+            return false;
+        }
+
+        if ($targetVariable['VariableType'] != 1 /* Integer */) {
+            return false;
+        }
+
+        $triggerValue = $value ? OPEN : CLOSE;
+
+        if (IPS_InstanceExists($profileAction)) {
+            IPS_RunScriptText('IPS_RequestAction(' . var_export($profileAction, true) . ', ' . var_export(IPS_GetObject($variableID)['ObjectIdent'], true) . ', ' . var_export($triggerValue, true) . ');');
+        } elseif (IPS_ScriptExists($profileAction)) {
+            IPS_RunScriptEx($profileAction, ['VARIABLE' => $variableID, 'VALUE' => $value, 'SENDER' => 'VoiceControl']);
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+}
